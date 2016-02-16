99ff8994e3dbf806dc86504cde9d7c87d1bde17f
Brent Luker <brent@brentluker.com>
2016-02-16 19:29:24 +0000

    Remov Sessions and Unused Imports
    The session isn't acutally being used yet, so it is removed for now. Some of the imports aren't being used either and are removed.

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ var express = require("express");
 var MongoClient = require('mongodb').MongoClient;
 var app = express();
 var assert = require("assert");
-var session = require('express-session');
-var ejs = require("ejs");
-//var ObjectId = require('mongodb').ObjectID;
-var genuuid = require("uuid");
-var cookieParser = require("cookie-parser");
-var queryString = require("query-string");
 var url = require('url');
 
-var dbCommands = require("./db-commands");
-var secrets = require("./secrets");
 var userRoutes = require("./routes/user");
+var db = require("db");
 
 
 
@@ -22,14 +15,6 @@ app.use(function(req, res, next){
 });
 
 app.set('view engine', 'ejs');
-app.use(cookieParser());
-app.use(session({ genid: function(req) {
-    return genuuid(); // use UUIDs for session IDs
-  },
-    secret: secrets.cookieSecret,
-    resave: true,
-    saveUninitialized: true
-}));
 app.use(express.static('./public'));
 
 
@@ -39,11 +24,11 @@ app.get("/form", function(req, res){
     res.render(__dirname+"/views/form", {data: query});
 });
 
+app.use('/api', userRoutes(db));
+
 MongoClient.connect("mongodb://"+process.env.IP+":27017/test", function(err, db){
     assert.equal(err, null, "Mongo had an error. ");
     console.log("Connected to database successfully.");
-    
-    app.use('/api', userRoutes(db));
     
     process.stdout.write("Starting server... ");
     app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
