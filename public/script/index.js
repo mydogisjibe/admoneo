a2987691dba32060b655c919fe5cfa7f8e37c74e
Brent Luker <brent@brentluker.com>
2016-02-17 00:22:30 +0000

    Clean Up Homepage
    On index.html, fix indenting, removed unused scripts and styles. On index.js, code that converts reminder json to html is converted back to JQuery instead of plain-text for the cleaner look. A few return false; statements that didn't do anything are removed.

--- public/script/index.js
+++ public/script/index.js
@@ -1,64 +1,57 @@
-//var link = "//admoneo-matthewgaim.c9users.io/form?reminder_name=potato&reminder_event=myEvent&reminder_message=This+is+my+message&reminder_email=bob%40bob.com&reminder_date=2016-02-14&reminder_id=0"
 function populateList(){
     $.get("/api/mydogisjibe", function (data) {
         
-
-         for (var i=0;i<data.length;i++){
-             var link = "/form?reminder_name="+encodeURIComponent(data[i].recipient)+"&reminder_event="+encodeURIComponent(data[i].event)+"&reminder_message="+encodeURIComponent(data[i].message)+"&reminder_email="+encodeURIComponent(data[i].email)+"&reminder_date="+encodeURIComponent(data[i].date)+"&reminder_id=0";
+        for (var i=0;i<data.length;i++){
+            var link = "/form?reminder_name="+encodeURIComponent(data[i].recipient)+"&reminder_event="+encodeURIComponent(data[i].event)+"&reminder_message="+encodeURIComponent(data[i].message)+"&reminder_email="+encodeURIComponent(data[i].email)+"&reminder_date="+encodeURIComponent(data[i].date)+"&reminder_id=0";
+            
+            var recipient  = $("<li>", {"class": "recipient" , text: "Recipient: "          + data[i].recipient});
+            var events     = $("<li>", {"class": "events"    , text: "Event: "              + data[i].event    });
+            var additional = $("<li>", {"class": "additional", text: "Additional Message: " + data[i].message  });
+            var email      = $("<li>", {"class": "email"     , text: "Email: "              + data[i].email    });
+            var date       = $("<li>", {"class": "date"      , text: "Date: "               + data[i].date     });
              
-             var recipient = '<li class="recipient">Recipient: '+data[i].recipient+'</li>';
-             var events = '<li class="events">Event: '+data[i].event+'</li>';
-             var additional = '<li class="additional">Additional Message: '+data[i].message+'</li>';
-             var email = '<li class="email">Email: '+data[i].email+'</li>';
-             var date = '<li class="date">Date: '+data[i].date+'</li>';
-             
-             var list = '<ul>'+recipient+events+additional+email+date+'</ul>';
-             var td = '<td  href="'+link+'" class="center" id="center'+i+'">'+list+'</td>';
-             var tr = '<tr><td class="left"><button class="delete btn btn-danger" id="button'+i+'" td>Delete</button>'+td+'</tr>';
-             
-             
-             $("table").append(tr);
-         }
+            var list         = $("<ul>").append(recipient, events, additional, email, date);
+            var listSquare   = $("<td>", {href: link, "class": "center", id: "center"+i}).append(list);
+            
+            var deleteButton = $("<button>", {"class": "delete btn btn-danger", id:"button"+i, text: "Delete"});
+            var deleteSquare = $("<td>", {"class": "left"}).append(deleteButton);
+            var tr   = $("<tr>").append(deleteSquare, listSquare);
+            
+            $("table").append(tr);
+        }
         
 
-           $(".delete").click(function(event){
-               event.preventDefault();
-                var num =($(this).attr('id')).substring(6,7);
-                num=parseInt(num,10);
-                $.ajax({
-                    url: '/api/mydogisjibe',
-                    type: 'delete',
-                    contentType: "application/json; charset=utf-8",
-                    dataType: 'json',
-                    success: function (data) {
-                        emptyList();
-                        populateList();
-                        return false;
-                    },
-                    data: JSON.stringify({remindNum: num})
-                });
-                return false;
+        $(".delete").click(function(event){
+            event.preventDefault();
+            var num = ($(this).attr('id')).substring(6,7);
+            num=parseInt(num,10);
+            $.ajax({
+                url: '/api/mydogisjibe',
+                type: 'delete',
+                contentType: "application/json; charset=utf-8",
+                dataType: 'json',
+                success: function (data) {
+                    emptyList();
+                    populateList();
+                },
+                data: JSON.stringify({remindNum: num})
             });
-            $("td").click(function(event) {
-                if($(this).attr("href") != undefined){
-                    window.location.href = $(this).attr("href");
-                }
-                else{
-                    return false;
-                }
-            });
-       
-       
+        });
             
-    function emptyList(){
-        $("td").remove();
-    } 
-       
-       
+        $("td").click(function(event) {
+            event.preventDefault();
+            if($(this).attr("href") != undefined){
+                window.location.href = $(this).attr("href");
+            }
+        });
+            
+        function emptyList(){
+            $("td").remove();
+        } 
+                   
     });
 }
 
 $(document).ready(function(){
-    
     populateList();
 });
